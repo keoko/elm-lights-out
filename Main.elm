9bc4379a3eba00dfe302e0acc1318fdf8ba9b0e1
Natxo <icabrebarrera@magento.com>
2016-08-10 20:27:32 +0200

    box with light

--- Main.elm
+++ Main.elm
@@ -1,12 +1,30 @@
-import Html exposing (div, h1, img, text)
+import Html exposing (div, h1, img, text, pre, hr)
 import Html.Attributes exposing (..)
+import Html.App exposing (beginnerProgram)
+import Html.Events exposing (onClick)
 
 main =
-    view "todo"
+  beginnerProgram { model = model, view = view, update = update }
+
+type Msg = ToggleLight
+
+type alias Model =
+    {
+        on: Bool
+    }
+
+model : Model
+model =
+    { on = True }
+
+
+update msg model =
+    case msg of
+        ToggleLight -> { model | on = not model.on }
 
 view model =
-    div [style [ ("background", "blue")
+    div [ style [ ("background", if model.on then "blue" else "black" )
                , ("width", "10vw")
                , ("height", "10vw")
                ]
-        ] []
+        , onClick ToggleLight ] []
