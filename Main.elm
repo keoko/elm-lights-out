3801ae14dd0cb822ba16deba868e045d3de95076
Natxo <icabrebarrera@magento.com>
2016-08-10 20:34:29 +0200

    type signatures

--- Main.elm
+++ Main.elm
@@ -1,8 +1,9 @@
-import Html exposing (div, h1, img, text, pre, hr)
+import Html exposing (Html, div, h1, img, text, pre, hr)
 import Html.Attributes exposing (..)
 import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 
+main : Program Never
 main =
   beginnerProgram { model = model, view = view, update = update }
 
@@ -18,10 +19,12 @@ model =
     { on = True }
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         ToggleLight -> { model | on = not model.on }
 
+view : Model -> Html Msg
 view model =
     div [ style [ ("background", if model.on then "blue" else "black" )
                , ("width", "10vw")
