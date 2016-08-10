4f6d9b10c9a0340ea69993ba19d2deeca19b1cfc
Natxo <icabrebarrera@magento.com>
2016-08-10 20:16:04 +0200

    static box

--- Main.elm
+++ Main.elm
@@ -1,4 +1,12 @@
-import Html exposing (text)
+import Html exposing (div, h1, img, text)
+import Html.Attributes exposing (..)
 
 main =
-  text "Hello, World!"
+    view "todo"
+
+view model =
+    div [style [ ("background", "blue")
+               , ("width", "10vw")
+               , ("height", "10vw")
+               ]
+        ] []
